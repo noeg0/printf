4950c0b51e51023cf061e6ee8c6d2884a7915e46
noeg0 <ego.no@yandex.com>
2016-08-19 15:56:05 +0300

    Mesut KILIÇ

---
+++ mesut1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+void k(void)
+{
+	int x;
+	*(&x+2) -= 34;
+}
+void main(void)
+{
+	int x;
+	x=0;
+	printf("X=%d\n", ++x);
+	
+	if (x >= 10) {
+	
+        return;
+    	
+		     }
+	k();
+
+}
